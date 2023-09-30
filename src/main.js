06e552b012a35b4abf38dd2d438e54bee877b925
Andrew Dinh <andrewdinh74@yahoo.com>
2023-09-30 11:26:57 -0700

    Finish with main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
   try {
+    const token = core.getInput('token')
+    const title = core.getInput('title')
+    const body = core.getInput('body')
+    const assignees = core.getInput('token')
+
+    const octokit = github.getOctokit(token)
+
+    const response = await octokit.rest.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined
+    })
+
+    core.setOutput('issue', response.data)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
